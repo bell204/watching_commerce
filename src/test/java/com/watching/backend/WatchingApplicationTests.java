1c521aa9ad7fcf59ed45f1779af8b328fd0ac576
bell204 <bell204@naver.com>
2023-12-15 22:42:13 +0900

    build.gradle java 11버전에 맞게 변경 gradle 버전 7.0.2 버전에 맞게 변경

--- src/test/java/com/watching/backend/WatchingApplicationTests.java
+++ src/test/java/com/watching/backend/WatchingApplicationTests.java
@@ -1,13 +1,13 @@
-package com.musinsa.watcher;
+package com.watching.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WatcherApplicationTests {
+class WatchingApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+	@Test
+	void contextLoads() {
+	}
 
 }
