f81fef2f8c006fd616b5b76d67eb320f6b8856c9
ljb2023 <bell2016@businesson.co.kr>
2023-12-18 11:56:45 +0900

    github action yml 파일 및 테스트 컨트롤러 파일 생성

---
+++ src/main/java/com/watching/backend/web/HomeController.java
@@ -0,0 +1,11 @@
+package com.watching.backend.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+
+    @GetMapping("/api/v1")
+    public String findHome() {
+        return "v1 test";
+    }
+}
