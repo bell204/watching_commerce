d5f55b5b776631c4fa6bf74ff65a66642658b598
bell204 <bell204@naver.com>
2023-12-26 14:55:04 +0900

    1 nginx.conf 추가 2 yml에 nginx 복사 코드 추가 3 homeController의 api/profile에서 프로필 찾게 수정

--- src/main/java/com/watching/backend/web/HomeController.java
+++ src/main/java/com/watching/backend/web/HomeController.java
@@ -1,11 +1,24 @@
 package com.watching.backend.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class HomeController {
 
-    @GetMapping("/api/v1")
-    public String findHome() {
-        return "v1 test1";
+    private final Environment env;
+
+    @GetMapping("/api/profile")
+    public String findProfile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> devProfiles = Arrays.asList("dev");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        return profiles.stream().filter(devProfiles::contains).findAny().orElse(defaultProfile);
     }
 }
