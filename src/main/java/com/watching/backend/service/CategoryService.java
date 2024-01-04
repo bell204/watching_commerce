4b1687484e0de62051da18980a5ff173ffc7cc5d
bell204 <bell204@naver.com>
2024-01-04 22:09:59 +0900

    1 properties에 레디스 설정 정보 추가 / spring actuator 추가 2 categories 데이터를 캐시 목록에 추가 3 레디스 config 파일 생성 및 레디스 캐시매니저 추가 4 EnableCaching 어노테이션 추가

--- src/main/java/com/watching/backend/service/CategoryService.java
+++ src/main/java/com/watching/backend/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.watching.backend.service;
 import com.watching.backend.domain.category.Category;
 import com.watching.backend.domain.category.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    @Cacheable(value = "categories", key = "'store_category'")
     public List<Category> getAllCategory() {
         List<Category> categories =  categoryRepository.findAll();
         return categories;
